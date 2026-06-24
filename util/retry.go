c2e0367bd06423722f05efd5d474e5d1311b4d27
Josh Allen <jallen@slack-corp.com>
2026-06-23 23:18:33 -0400

    retry delay

--- util/retry.go
+++ util/retry.go
@@ -1,10 +1,14 @@
 package util
 
-import "regexp"
+import (
+	"regexp"
+	"time"
+)
 
 // Number of times to retry a "gh" command that fails. GitHub commands can fail
 // transiently, so they are retried on any error.
 const GhRetries = 2
+const RetryDelay = 1 * time.Second
 
 // Number of times to retry a "git" command that fails due to ".git/index.lock" contention.
 const IndexLockRetries = 3
@@ -15,12 +19,20 @@ var indexLockRegexp = regexp.MustCompile(`Unable to create '.*index\.lock': File
 // RetryOnIndexLock retries when a git command fails due to transient
 // ".git/index.lock" contention, up to [IndexLockRetries] times.
 func RetryOnIndexLock(executionCount int, stderr string) bool {
-	return executionCount <= IndexLockRetries && indexLockRegexp.MatchString(stderr)
+	retry := executionCount <= IndexLockRetries && indexLockRegexp.MatchString(stderr)
+	if retry {
+		Sleep(time.Duration(executionCount) * RetryDelay)
+	}
+	return retry
 }
 
 // RetryUpTo returns a [ShouldRetryFunc] that retries on any failure, up to maxRetries times.
 func RetryUpTo(maxRetries int) ShouldRetryFunc {
 	return func(executionCount int, _ string) bool {
-		return executionCount <= maxRetries
+		retry := executionCount <= maxRetries
+		if retry {
+			Sleep(time.Duration(executionCount) * RetryDelay)
+		}
+		return retry
 	}
 }
