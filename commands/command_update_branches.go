ada45f602f22c029ffdc9c585fe408281b128668
Josh Allen <jallen@slack-corp.com>
2026-07-07 12:43:49 -0400

    Upgrade stable to Version 2.1.7

--- commands/command_update_branches.go
+++ commands/command_update_branches.go
@@ -18,6 +18,10 @@ func createUpdateBranchesCommand() *cobra.Command {
 		Short: "Update PR branches with contents of the local " + gitutil.GetMainBranchForHelp() + " commit",
 		Long: "Update PR branches so they match the contents of local " + gitutil.GetMainBranchForHelp() + ".\n" +
 			"\n" +
+			"This is the opposite of `replace-commit`. It updates PR branches to the contents of the corresponding \n" +
+			"commits on local main and pushes to remote. Use this after `sd rebase-main` to update your PR \n" +
+			"branches. Uses `git rebase` if the PR is in draft status, or `git merge` if the PR is open.\n" +
+			"\n" +
 			"Shows a selection dialog with PR branches whose diffs differ from the\n" +
 			"commit on local " + gitutil.GetMainBranchForHelp() + ". Branches already in sync are not selectable.\n" +
 			"\n" +
