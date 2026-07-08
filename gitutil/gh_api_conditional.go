0559f993ef9604bd09f785dedb68c83bdbbadb44
Josh Allen <jallen@slack-corp.com>
2026-07-07 23:48:18 -0400

    Use HEAD (#105)

--- gitutil/gh_api_conditional.go
+++ gitutil/gh_api_conditional.go
@@ -16,7 +16,7 @@ func fetchPRWithETag(prNumber int, etag string) string {
 	nameWithOwner := GetRepoNameWithOwner()
 	endpoint := fmt.Sprintf("/repos/%s/pulls/%d", nameWithOwner, prNumber)
 
-	args := []any{"api", "--include", "--hostname", GetRepoHostname()}
+	args := []any{"api", "--method", "HEAD", "--include", "--hostname", GetRepoHostname()}
 	if etag != "" {
 		args = append(args, "-H", "If-None-Match: "+etag)
 	}
